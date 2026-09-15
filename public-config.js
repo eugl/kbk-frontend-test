323115f20fa3e7c35f56bd0d9409d0000511ec69
eugl <36508398+eugl@users.noreply.github.com>
2026-09-16 00:45:05 +0200

    Add files via upload

---
+++ public-config.js
@@ -0,0 +1,10 @@
+window.KBK_APP_CONFIG = {
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwTrPwal1q8UrtkuxEsUlcG8_ct28tZaw-3d8qs4RXcCHL1Ei-ZATWzlc_ly2_lt5Ho_w/exec'
+};
+
+window.KBK_PUBLIC_SHEET_IDS = {
+  cennikFileId: '1nzGOdgjHUhpywiJ2AtcFHhoa3dsb-WuejhhBKulDLEE',
+  wnioskiFileId: '1ecI-RVM_7Jwg8Z2r8JCSP7DKYBo9sSMl1PYGTbtd07o',
+  transakcjeFileId: '1Y9F7WdB_RC6f3lA4zz_WpUbrzsFEEnqKNh3tfSRdmoA',
+  transakcjeArchiveFileId: ''
+};
